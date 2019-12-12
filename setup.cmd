553965847158cd74ca496decf87197a146edcfbb
cboudereau <clement.boudereau@availpro.com>
2019-12-12 16:02:58 +0100

    clean

--- setup.cmd
+++ setup.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-call installdeps.cmd && docker-compose run -d --rm -p 3306:3306 --name localdb db 2>NUL && goto MYSQL_CHECK
+call installdeps.cmd && docker-compose run -d --rm -p 3306:3306 --name localdb db || goto MYSQL_CHECK
 
 :MYSQL_CHECK
 docker logs localdb 2>&1 | find /I "MySQL init process done. Ready for start up."
